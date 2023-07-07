ca0ccc387765922f4b1a599f3e84caaf99e68739
Muchukota-Shiva <500091913@stu.upes.ac.in>
2023-07-08 01:01:20 +0530

    created timer file, added fields

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -3,9 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// A timer
+/// A timer that can be added as a component
 /// </summary>
 public class Timer : MonoBehaviour
 {
-	// place holder for now
+	// fields to be changed and used by methods
+    #region Fields
+	
+	// timer duration
+	float totalSeconds = 0;
+	
+	// timer execution
+	float elapsedSeconds = 0;
+	bool running = false;
+	
+	// support for Finished property
+	bool started = false;
+	
+	#endregion
+
+	/// 
+	/// need to add methods next
+	///
 }
