677f33e33c46f85108e146089ec8c3a3994ce749
Muchukota-Shiva <500091913@stu.upes.ac.in>
2023-07-08 01:18:42 +0530

    made changes in timer file

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,22 +7,22 @@ using UnityEngine;
 /// </summary>
 public class Timer : MonoBehaviour
 {
-	// fields to be changed and used by methods
+    // fields to be changed and used by methods
     #region Fields
-	
-	// timer duration
-	float totalSeconds = 0;
-	
-	// timer execution
-	float elapsedSeconds = 0;
-	bool running = false;
-	
-	// support for Finished property
-	bool started = false;
-	
-	#endregion
 
-	/// 
-	/// need to add methods next
-	///
+    // timer duration
+    float totalSeconds = 0;
+
+    // timer execution
+    float elapsedSeconds = 0;
+    bool running = false;
+
+    // support for Finished property
+    bool started = false;
+
+    #endregion
+
+    /// 
+    /// need to add methods next
+    ///
 }
