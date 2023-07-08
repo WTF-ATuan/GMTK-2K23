c189bbf6cd4a43cb066e699150994706ee73959d
Muchukota-Shiva <500091913@stu.upes.ac.in>
2023-07-08 10:31:38 +0530

    finished the Timer.cs file

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -61,5 +61,40 @@ public class Timer : MonoBehaviour
 	}
 
     #endregion
+    
+    #region Methods
 
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {	
+		// update timer and check for finished
+		if (running)
+        {
+			elapsedSeconds += Time.deltaTime;
+			if (elapsedSeconds >= totalSeconds)
+            {
+				running = false;
+			}
+		}
+	}
+	
+	/// <summary>
+	/// Runs the timer because a timer of 0 duration doesn't really make sense,
+	/// the timer only runs if the total seconds is larger than 0
+	/// This also makes sure the consumer of the class has actually set the duration to something higher than 0
+	/// </summary>
+	public void Run()
+    {	
+		// only run with valid duration
+		if (totalSeconds > 0)
+        {
+			started = true;
+			running = true;
+            elapsedSeconds = 0;
+		}
+	}
+	
+	#endregion
 }
