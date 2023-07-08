beab76f0d68e1536e39fb579743f185d9de53071
Muchukota-Shiva <500091913@stu.upes.ac.in>
2023-07-08 10:14:48 +0530

    added properties to timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -25,4 +25,41 @@ public class Timer : MonoBehaviour
     /// 
     /// need to add methods next
     ///
+	#region Properties
+	
+	/// <summary>
+	/// Sets the duration of the timer the duration can only be set if the timer isn't currently running
+	/// </summary>
+	/// <value>duration</value>
+	public float Duration
+    {
+		set
+        {
+			if (!running)
+            {
+				totalSeconds = value;
+			}
+		}
+	}
+	
+	/// <summary>
+	/// Gets whether or not the timer has finished running this property returns false if the timer has never been started
+	/// </summary>
+	/// <value>true if finished; otherwise, false.</value>
+	public bool Finished
+    {
+		get { return started && !running; } 
+	}
+	
+	/// <summary>
+	/// Gets whether or not the timer is currently running
+	/// </summary>
+	/// <value>true if running; otherwise, false.</value>
+	public bool Running
+    {
+		get { return running; }
+	}
+
+    #endregion
+
 }
