1f79d410b4c9174403de48b12a40245b2594d88c
ATuan <4A8K0069@stust.edu.tw>
2023-07-09 16:16:09 +0800

    updated drag and drop to UI

--- Assets/ScriptableObject/ListController.cs
+++ Assets/ScriptableObject/ListController.cs
@@ -4,46 +4,42 @@ using UnityEngine;
 using UnityEngine.UI;
 using static Unity.Collections.AllocatorManager;
 
-public class ListController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    public BlockList list;
-    public List<Block> instantiatedBlock;
-    public List<Block> blocks;
-    public Transform container;
-    public GameObject blockPrefab;
-    float timer;
-    float cd = 10;
+public class ListController : MonoBehaviour{
+	// Start is called before the first frame update
+	public BlockList list;
+	public List<Block> instantiatedBlock;
+	public List<Block> blocks;
+	public Transform container;
+	public GameObject blockPrefab;
+	float timer;
+	[SerializeField] private float cd = 10;
 
-    void Start()
-    {
-        foreach (var item in blocks) {
-            InitBlock(item);
-        }
-        timer = cd;
-    }
+	void Start(){
+		foreach(var item in blocks){
+			InitBlock(item);
+		}
 
-    // Update is called once per frame
-    void Update()
-    {
-        timer -= Time.deltaTime;
-        if (timer <= 0)
-        {
-            timer = cd;
-            Add(2);
-            InitBlock(blocks[index: blocks.Count - 1]);
-        }
-    }
+		timer = cd;
+	}
 
-    public void InitBlock(Block block)
-    {
-        GameObject blockContainer = Instantiate(blockPrefab, container);
-        blockContainer.GetComponent<Image>().sprite = block.sprite;
-        blockContainer.GetComponent<BlockControl>().holdingBlock = block;
-    }
+	// Update is called once per frame
+	void Update(){
+		timer -= Time.deltaTime;
+		if(timer <= 0){
+			timer = cd;
+			Add(2);
+			InitBlock(blocks[index: Random.Range(1, blocks.Count)]);
+		}
+	}
 
-    public void Add(int id)
-    {
-        blocks.Add(new Block(instantiatedBlock[id].ID, instantiatedBlock[id].summonedBlock, instantiatedBlock[id].sprite));
-    }
+	public void InitBlock(Block block){
+		GameObject blockContainer = Instantiate(blockPrefab, container);
+		blockContainer.GetComponent<Image>().sprite = block.sprite;
+		blockContainer.GetComponent<BlockControl>().holdingBlock = block;
+	}
+
+	public void Add(int id){
+		blocks.Add(new Block(instantiatedBlock[id].ID, instantiatedBlock[id].summonedBlock,
+			instantiatedBlock[id].sprite));
+	}
 }
--- Assets/Scripts/BlockControl.cs
+++ Assets/Scripts/BlockControl.cs
@@ -1,8 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class BlockControl : MonoBehaviour
-{
-    public Block holdingBlock;
+public class BlockControl : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler{
+	public Block holdingBlock;
+
+
+	private CanvasGroup _canvasGroup;
+	private Vector2 _deviation;
+	private GameObject _cloneGameObject;
+
+	private void Awake(){
+		_canvasGroup = GetComponent<CanvasGroup>();
+	}
+
+	public void OnBeginDrag(PointerEventData eventData){
+		_deviation = (Vector2)transform.localPosition - eventData.position;
+		var worldPoint = Camera.main.ScreenToWorldPoint(eventData.position);
+		worldPoint.z = 0;
+		_cloneGameObject = Instantiate(holdingBlock.summonedBlock, worldPoint, Quaternion.identity);
+		_canvasGroup.blocksRaycasts = false;
+		_canvasGroup.alpha = 0.7f;
+	}
+
+	public void OnEndDrag(PointerEventData eventData){
+		_canvasGroup.blocksRaycasts = true;
+		_canvasGroup.alpha = 1f;
+	}
+
+	public void OnDrag(PointerEventData eventData){
+		transform.localPosition = eventData.position + _deviation;
+		var worldPoint = (Vector2)Camera.main.ScreenToWorldPoint(eventData.position);
+		_cloneGameObject.transform.position = worldPoint;
+	}
 }
